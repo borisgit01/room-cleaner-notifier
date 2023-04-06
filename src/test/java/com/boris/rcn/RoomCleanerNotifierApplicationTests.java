66f2746b1e0710bf6298163a73edbec0c7ee0037
borisgit01 <borisgodunovph@gmail.com>
2023-04-06 11:37:54 -0400

    initial commit 2

---
+++ src/test/java/com/boris/rcn/RoomCleanerNotifierApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boris.rcn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoomCleanerNotifierApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
